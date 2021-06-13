3fa63857103395f7cb493a2e7d62b5586b769ffa
wangtianbao <wangtianbao2014@sina.com>
2021-06-13 14:16:29 +0800

    Update

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ async function start() {
   console.log('下载代码完毕')
   // 替换变量
   await changeFiele();
-  console.log('替换变量完毕')
+  console.log('替换变量完毕');
   // 执行
   await exec("node JD_DailyBonus.js >> result.txt");
   console.log('执行完毕')
